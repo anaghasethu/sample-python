668e3b4f279166e9c62927d527e7f25ab6f8c926
anaghasethu <anaghasethu1998@gmail.com>
2022-04-18 17:11:32 +0530

    first commit - length

---
+++ string-length.py
@@ -0,0 +1,7 @@
+string = input("Enter the string: ")
+
+print("Length using build-in function: ",len(string))
+length=0
+for i in string:
+   length = length + 1 
+print("Length without using build-in function: ",length)
