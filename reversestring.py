3d276ccc83e697cce9f3e22266dd8a6abb9a350c
anaghasethu <anaghasethu1998@gmail.com>
2022-04-18 17:51:11 +0530

    updated - reverse string

--- reversestring.py
+++ reversestring.py
@@ -1,2 +1,6 @@
 string = input("Enter the string: ")
 print("Reversed string:",string[::-1])
+result = ""
+for i in range(0,len(string)):
+    result =  string[i] + result
+print("Reversed using loop: ", result)
